b1ab8117ca09e7b6ebd95674bf12735462ebf852
Milica Milovanovic <milica.milovanovic01@gmail.com>
2023-01-18 20:55:54 +0100

    Napravljena get ruta

--- poslasticarnica/routes/api.php
+++ poslasticarnica/routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', [ProductController::class,'index']);
+Route::get('/products/{id}', [ProductController::class,'show']);
+    // Route::resource('products', ProductController::class);
+    // Route::resource('categories', CategoryController::class);
+    // Route::resource('users', UserController::class);
+
+// Route::post('/register',[AuthController::class,'register']);
+// Route::post('/login',[AuthController::class,'login']);
+
+
+//  Route::get('sneakers/brand/{id}',[SneakersController::class,'getByBrand']);
+
+//  Route::get('sneakers/type/{id}',[SneakersController::class,'getByType']);
+
+
+//  Route::group(['middleware' => ['auth:sanctum']], function () {
+//     Route::get('/profile', function(Request $request) {
+//         return auth()->user();
+//     });
+
+//     Route::get('my-sneakers',[SneakersController::class,'mySneakers']);
+
+//     Route::get('/logout',[AuthController::class,'logout']);
+
+//     Route::resource('sneakers',SneakersController::class)->only('store','update','destroy');
+// });
