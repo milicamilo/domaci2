0b43928f939c6d1455810c8a6764e5a31b83d761
Milica Milovanovic <milica.milovanovic01@gmail.com>
2023-02-08 16:42:05 +0100

    Sredjen kod

--- poslasticarnica/routes/api.php
+++ poslasticarnica/routes/api.php
@@ -29,26 +29,14 @@ Route::get('/categories', [CategoryController::class,'index']);
 Route::get('/products/{id}', [ProductController::class,'show']);
 Route::get('/products/categories/{category_id}', [ProductController::class,'getByCategories']);
 Route::get('/products/users/{user_id}', [ProductController::class,'getByUsers']);
-    // Route::resource('categories', CategoryController::class);
-    // Route::resource('users', UserController::class);
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
-
-
-//  Route::get('sneakers/brand/{id}',[SneakersController::class,'getByBrand']);
-
-//  Route::get('sneakers/type/{id}',[SneakersController::class,'getByType']);
-
-
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
     Route::resource('products', ProductController::class)->only(['store', 'update', 'destroy']);
-
     Route::post('products',[ProductController::class,'store']);
     Route::put('products/{id}',[ProductController::class,'update']);
     Route::delete('products/{id}',[ProductController::class,'destroy']);
-
     Route::get('myProduct',[ProductController::class,'myProduct']);
-
     Route::post('/logout',[AuthController::class,'logout']);
 });
